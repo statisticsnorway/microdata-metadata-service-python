ef3e28b8368917a2bcb2b9a3a47c75b917c50c47
DanielElisenberg <Daniel@Elisenberg.com>
2024-07-29 14:22:33 +0200

    new_json_logging: fix logging config

--- metadata_service/config/logging.py
+++ metadata_service/config/logging.py
@@ -1,8 +1,11 @@
-import json
 import sys
+import uuid
+import json
+import logging
+import datetime as dt
 
-import json_logging
 import tomlkit
+from flask import request, g
 
 from metadata_service.config import environment
 
@@ -14,53 +17,81 @@ def _get_project_meta():
     return tomlkit.parse(file_contents)["tool"]["poetry"]
 
 
-pkg_meta = _get_project_meta()
-host = environment.get("DOCKER_HOST_NAME")
-command = json.dumps(sys.argv)
+class RequestInfoFilter(logging.Filter):
+    def filter(self, record: logging.LogRecord) -> bool:
+        record.correlation_id = request.headers.get("X-Request-ID", "unknown")
+        record.method = request.method
+        record.url = request.url
+        record.remote_host = request.remote_addr
+        record.response_status = getattr(g, "response_status", "unknown")
+        record.response_time_ms = getattr(g, "response_time_ms", "unknown")
+        return True
 
 
-class CustomJSONLog(json_logging.JSONLogWebFormatter):
-    """
-    Customized application logger
-    """
+class MicrodataJSONFormatter(logging.Formatter):
+    def __init__(self):
+        self.pkg_meta = _get_project_meta()
+        self.host = environment.get("DOCKER_HOST_NAME")
+        self.command = json.dumps(sys.argv)
 
-    def _format_log_object(self, record, request_util):
-        json_log_object = super(CustomJSONLog, self)._format_log_object(
-            record, request_util
+    def format(self, record: logging.LogRecord) -> str:
+        return json.dumps(
+            {
+                "@timestamp": dt.datetime.fromtimestamp(
+                    record.created,
+                    tz=dt.timezone.utc,
+                ).isoformat(),
+                "command": self.command,
+                "error.stack": record.__dict__.get("exc_info"),
+                "host": self.host,
+                "message": record.getMessage(),
+                "level": record.levelno,
+                "levelName": record.levelname,
+                "loggerName": record.name,
+                "method": record.__dict__.get("method"),
+                "responseTime": record.__dict__.get("response_time_ms"),
+                "schemaVersion": "v3",
+                "serviceName": "metadata-service",
+                "serviceVersion": str(self.pkg_meta["version"]),
+                "source_host": record.__dict__.get("remote_host"),
+                "statusCode": record.__dict__.get("response_status"),
+                "thread": record.threadName,
+                "url": record.__dict__.get("request"),
+                "xRequestId": record.__dict__.get("correlation_id"),
+            }
         )
-        return create_microdata_json_log(json_log_object, record)
 
 
-class CustomJSONRequestLogFormatter(json_logging.JSONRequestLogFormatter):
-    """
-    Customized request logger
-    """
+def setup_logging(app, log_level: int = logging.INFO) -> None:
+    """Set up logging configuration."""
+    logger = logging.getLogger()
+    logger.setLevel(log_level)
 
-    def _format_log_object(self, record, request_util):
-        json_log_object = super(
-            CustomJSONRequestLogFormatter, self
-        )._format_log_object(record, request_util)
-        return create_microdata_json_log(json_log_object, record)
+    formatter = MicrodataJSONFormatter()
 
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    request_info_filter = RequestInfoFilter()
+    logger.addFilter(request_info_filter)
+    logger.addHandler(stream_handler)
 
-def create_microdata_json_log(json_log_object, record):
-    return {
-        "@timestamp": json_log_object["written_at"],
-        "command": command,
-        "error.stack": json_log_object.get("exc_info"),
-        "host": host,
-        "message": record.getMessage(),
-        "level": record.levelno,
-        "levelName": record.levelname,
-        "loggerName": record.name,
-        "method": json_log_object.get("method"),
-        "responseTime": json_log_object.get("response_time_ms"),
-        "schemaVersion": "v3",
-        "serviceName": "metadata-service",
-        "serviceVersion": str(pkg_meta["version"]),
-        "source_host": json_log_object.get("remote_host"),
-        "statusCode": json_log_object.get("response_status"),
-        "thread": record.threadName,
-        "url": json_log_object.get("request"),
-        "xRequestId": json_log_object.get("correlation_id"),
-    }
+    @app.before_request
+    def before_request():
+        g.start_time = dt.datetime.now()
+        correlation_id = request.headers.get("X-Request-ID", None)
+        if correlation_id is None:
+            g.correlation_id = "metadata-service-" + str(uuid.uuid1())
+        else:
+            g.correlation_id = correlation_id
+        g.method = request.method
+        g.url = request.url
+        g.remote_host = request.remote_addr
+
+    @app.after_request
+    def after_request(response):
+        g.response_time_ms = int(
+            (dt.datetime.now() - g.start_time).total_seconds() * 1000
+        )
+        g.response_status = response.status_code
+        response.headers["X-Request-ID"] = g.correlation_id
+        return response
